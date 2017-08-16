4e71344bb81b990f9d718e57229245e7c4887a64
João Paulo <Joo Paulo@192.168.86.100>
2017-08-16 10:16:06 -0300

    Adicionando o IP do banco de dados;

--- src/main/java/br/com/joaops/site/config/PersistenceConfig.java
+++ src/main/java/br/com/joaops/site/config/PersistenceConfig.java
@@ -31,7 +31,7 @@ public class PersistenceConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/sampledb");
+        dataSource.setUrl("jdbc:postgresql://172.30.213.205:5432/sampledb");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
         return dataSource;
