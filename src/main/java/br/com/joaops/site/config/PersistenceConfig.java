23452ff8de192495e7f1b7ad355dde6f152289ab
João Paulo <Joo Paulo@192.168.86.100>
2017-08-16 11:53:48 -0300

    Adicionando número IP do BD;

--- src/main/java/br/com/joaops/site/config/PersistenceConfig.java
+++ src/main/java/br/com/joaops/site/config/PersistenceConfig.java
@@ -29,10 +29,16 @@ public class PersistenceConfig {
     
     @Bean
     public DataSource dataSource() {
+        try {
+            System.out.println("Banco de Dados: " + System.getenv("POSTGRESQL_DATABASE"));
+            System.out.println("Usuário do Banco de Dados: " + System.getenv("POSTGRESQL_USER"));
+            System.out.println("Senha do Banco de Dados: " + System.getenv("POSTGRESQL_PASSWORD"));
+        } catch (Exception e) {
+            System.err.println("ERRO " + e);
+        }
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        //dataSource.setUrl("jdbc:postgresql://172.30.213.205:5432/sampledb");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/sampledb");
+        dataSource.setUrl("jdbc:postgresql://172.31.50.220:5432/sampledb");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
         return dataSource;
