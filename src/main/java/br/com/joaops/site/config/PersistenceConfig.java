717d1b2ab229dc56e4c630a93cf983c6f7886ace
João Paulo <Joo Paulo@192.168.86.100>
2017-08-16 09:25:39 -0300

    Alterando nome do banco de dados;

--- src/main/java/br/com/joaops/site/config/PersistenceConfig.java
+++ src/main/java/br/com/joaops/site/config/PersistenceConfig.java
@@ -31,7 +31,7 @@ public class PersistenceConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost/site");
+        dataSource.setUrl("jdbc:postgresql://localhost/sampledb");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
         return dataSource;
